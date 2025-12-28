ffb62f2057d14a0f11a1ebabfb1635a8d35e3d1e
Jonathan Ibanez <jcharlesibanez@gmail.com>
2025-12-27 21:11:33 -0500

    Prepared svg, added some other quality of life improvements.

--- scholarium/src/App.jsx
+++ scholarium/src/App.jsx
@@ -1,4 +1,10 @@
 import React, { useState } from 'react'
+import prompt from "./prompt.js"
+import OpenAI from "openai";
+const client = new OpenAI({
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true
+});
 
 import Header from "./Header.jsx"
 import Background from "./Background.jsx"
@@ -9,15 +15,33 @@ function App() {
   const [value, setValue] = useState("");
   const [panelVisible, setPanelVisible] = useState(false);
 
-  const handleGo = (e) => {
-    e?.preventDefault();
-    setValue("Loading...");
+  const handleGo = async (userInput) => {
     setPanelVisible(true);
+    let dotCount = 0;
+    const loadingInterval = setInterval(() => {
+      dotCount = (dotCount + 1) % 4;
+      setValue("Loading" + ".".repeat(dotCount));
+    }, 500);
+
+    try {
+      const response = await client.responses.create({
+        model: 'gpt-5-nano',
+        input: prompt+userInput
+      });
+
+      prompt += `User:${userInput}\nYou:${response}\n`;
+
+      setValue(response.output_text);
+    } catch { setValue("An error occured in fetching a response..")
+    } finally {
+      clearInterval(loadingInterval);
+    }
   };
 
   const handlePanelClose = (e) => {
     e?.preventDefault();
     setPanelVisible(false);
+    setValue("");
   }
 
   return (
@@ -25,7 +49,7 @@ function App() {
     <Header visible={panelVisible}/>
     <Background/>
     <Panel text={value} visible={panelVisible} onClose={handlePanelClose} />
-    <EntryBar onSubmit={handleGo} />
+    <EntryBar onSubmit={handleGo} lowered={panelVisible} />
     </>
   );
 }
