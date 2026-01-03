2be263ae9622fa9b307df5093982aeaa97d9c565
Jonathan Ibanez <jcharlesibanez@gmail.com>
2026-01-03 14:37:19 -0500

    Animation proto-functionality and added logo

--- scholarium/src/prompt.js
+++
@@ -1,9 +0,0 @@
-const prompt = `
-You're an educator and a toddler comes up to you and says to you the following message, and you must use as little words as possible while still being
-coherent.
-
-Conversation History:
-
-`;
-
-export default prompt;
